5e83cd5f737f7ede60066253ee0141e7948056c8
BootesVoid <78485654+AMohamedAakhil@users.noreply.github.com>
2023-05-09 22:15:22 +0530

    fixed all 3

--- cmagcm.py
+++ cmagcm.py
@@ -8,6 +8,7 @@ import subprocess
 import sys
 from datetime import date, timedelta
 import calendar
+import random
 
 def cma(from_city, to_city, day, month, container, weight, number_containers, commodity, soc=False, time_small=1, time_med=2, time_large=3):
     
@@ -22,34 +23,35 @@ def cma(from_city, to_city, day, month, container, weight, number_containers, co
     while True:
         try:
             driver.find_element("xpath", "/html/body/div[2]/div/main/div/section/form/fieldset[1]/div/div[1]/div[2]/div/div/div[1]/input").send_keys(from_city)  # From city
-            sleep(time_med)
+            wait = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "el-scrollbar__view.el-autocomplete-suggestion__list")))
+            sleep(0.5)
             li_elements = driver.find_elements(By.TAG_NAME, "li")
             li_elements[-1].click()
-    
+            sleep(random.uniform(0, 0.4))
+            
             driver.find_element("xpath", "/html/body/div[2]/div/main/div/section/form/fieldset[1]/div/div[1]/div[3]/div/div/div[1]/input").send_keys(to_city)  # To city
-            sleep(time_med)
+            wait = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "el-scrollbar__view.el-autocomplete-suggestion__list")))
+            sleep(0.5)
             li_elements = driver.find_elements(By.TAG_NAME, "li")
             li_elements[-1].click()
-            sleep(0.3)
+            sleep(random.uniform(0, 0.4))
             # Clicking on date picker
             driver.find_element("xpath", "/html/body/div[2]/div/main/div/section/form/fieldset[1]/div/div[2]/div/div/div/input").click()
-            sleep(0.3)
+            sleep(random.uniform(0, 0.4))
             current_month = driver.find_element(By.CSS_SELECTOR, "#body > div.el-picker-panel.el-date-picker.el-popper.has-sidebar > div.el-picker-panel__body-wrapper > div.el-picker-panel__body > div.el-date-picker__header > span:nth-child(4)").text
-            sleep(0.7)
+            sleep(random.uniform(0, 1))
             if current_month == month:
                 # retrieving all available dates
                 dates = driver.find_elements(By.CLASS_NAME, "available")
                 for i in dates:
-                    print(i.text)
                     if i.text == day:
                         i.click()
                         break
             else:
-                print("current month not month")
                 driver.find_element(By.CSS_SELECTOR, "#body > div.el-picker-panel.el-date-picker.el-popper.has-sidebar > div.el-picker-panel__body-wrapper > div.el-picker-panel__body > div.el-date-picker__header > button.el-picker-panel__icon-btn.el-date-picker__next-btn.el-icon-arrow-right").click()
                 driver.find_element(By.CSS_SELECTOR, "#body > div.el-picker-panel.el-date-picker.el-popper.has-sidebar > div.el-picker-panel__body-wrapper > div.el-picker-panel__body > div.el-date-picker__header > button.el-picker-panel__icon-btn.el-date-picker__next-btn.el-icon-arrow-right").click()
                 next_month = driver.find_element(By.CSS_SELECTOR, "#body > div.el-picker-panel.el-date-picker.el-popper.has-sidebar > div.el-picker-panel__body-wrapper > div.el-picker-panel__body > div.el-date-picker__header > span:nth-child(4)").text
-                sleep(0.5)
+                sleep(random.uniform(0, 0.4))
                 if next_month == month:
                     new_dates = driver.find_elements(By.CLASS_NAME, "available")
                     for i in new_dates:
@@ -58,10 +60,10 @@ def cma(from_city, to_city, day, month, container, weight, number_containers, co
                             break
                 else:
                     print("Date not available")
-            
+            sleep(random.uniform(0, 0.2))
             if soc:
                 driver.find_element("xpath", "/html/body/div[2]/div/main/div/section/form/fieldset[2]/div[1]/div/div/span[2]").click()  # SOC
-            
+            sleep(random.uniform(0, 0.2))
             driver.find_element("xpath", "/html/body/div[2]/div/main/div/section/form/fieldset[2]/div[2]/div[1]/div/div/div/input").click() # Clicking on Equiment Type
             equipment_types = driver.find_elements(By.CLASS_NAME, "el-select-dropdown__item")
             for i in equipment_types:
@@ -72,6 +74,7 @@ def cma(from_city, to_city, day, month, container, weight, number_containers, co
             
             weight_box = driver.find_element("xpath", "/html/body/div[2]/div/main/div/section/form/fieldset[2]/div[2]/div[2]/div").click()
             driver.find_element("xpath", "/html/body/div[2]/div/main/div/section/form/fieldset[2]/div[2]/div[2]/div/span/input").send_keys(weight)
+            sleep(random.uniform(0, 0.2))
 
             for i in range(int(number_containers)-1):
                 num_containers = driver.find_element("xpath", "/html/body/div[2]/div/main/div/section/form/fieldset[2]/div[2]/div[3]/div/div/button[2]").click()
@@ -84,6 +87,7 @@ def cma(from_city, to_city, day, month, container, weight, number_containers, co
                 if i.text == commodity:
                     i.click()
                     break
+            sleep(random.uniform(0, 0.2))
 
             driver.find_element("xpath", "/html/body/div[2]/div/main/div/section/form/div/div/button[1]").click() # Submit Button	
 
@@ -97,10 +101,8 @@ def cma(from_city, to_city, day, month, container, weight, number_containers, co
                 sleep(time_large)
                 continue
             except:
-                print("Solve captcha !")
-                wait = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/main/div/section/form/fieldset[1]/div/div[1]/div[2]/div/div/div[1]/input")))
-                sleep(time_large)
-                continue
+                print("Solve captcha and rerun!")
+                break
 
 
 todays_date = date.today()
@@ -109,3 +111,4 @@ day = two_weeks.day
 month = calendar.month_abbr[two_weeks.month]
 
 cma(from_city=sys.argv[1], to_city=sys.argv[2], day = str(day), month = str(month), container = "20' Dry Standard", weight = "22000", number_containers = "1", commodity="Freight All Kinds")
+#cma(from_city="chennai", to_city="norfolk", day = str(day), month = str(month), container = "20' Dry Standard", weight = "22000", number_containers = "1", commodity="Freight All Kinds")
--- happag.py
+++ happag.py
@@ -23,7 +23,8 @@ def happag(from_city, to_city, date, timesmall=1, timemed=3, timelarge=5):
     while True:
         try:
             # #azureFormSpinner > svg
-            driver.get("https://solutions.hapag-lloyd.com/quick-quotes/#/")
+            
+            driver.get("https://solutions.hapag-lloyd.com/quick-quotes")
             elem = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "desktop.no-touch.body--light")))
             sleep(4)
             start_loc = driver.find_element("xpath", "/html/body/div[2]/div/div/div/div/form/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div/div/div[1]/label/div/div[1]/div[2]/div[1]/input")
@@ -77,31 +78,59 @@ def happag(from_city, to_city, date, timesmall=1, timemed=3, timelarge=5):
         except:
      
             try:
-                wait = WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[2]/iframe")))
-                iframe = driver.find_element("xpath", "/html/body/div[2]/div/div[2]/iframe")
+                try:
+                    driver.find_element(By.CSS_SELECTOR, "#onetrust-pc-sdk > div.ot-button-group-parent > div.ot-button-group > button.save-preference-btn-handler.onetrust-close-btn-handler").click()
+                    
+                    wait = WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[2]/iframe")))
+                    iframe = driver.find_element("xpath", "/html/body/div[2]/div/div[2]/iframe")
 
-                driver.switch_to.frame(iframe)
-                wait = WebDriverWait(driver, 8).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div")))
-                driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[1]/input').click()
-                driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[1]/input').click()
-                driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[1]/input').send_keys("timescantaufeek")
+                    driver.switch_to.frame(iframe)
+                    wait = WebDriverWait(driver, 8).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div")))
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[1]/input').click()
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[1]/input').click()
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[1]/input').send_keys("timescantaufeek")
 
-                #/html/body/div[2]/div/div/form/div[3]/div[2]/input
+                    #/html/body/div[2]/div/div/form/div[3]/div[2]/input
 
-                driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[2]/input').click()
-                driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[2]/input').click()
-                driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[2]/input').send_keys("TIMESCAN")
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[2]/input').click()
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[2]/input').click()
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[2]/input').send_keys("TIMESCAN")
 
-                driver.find_element(By.XPATH, "/html/body/div[2]/div/div/form/div[3]/div[4]/button").click()
-                driver.switch_to.default_content()
-                wait = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div/form/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div/div/div[1]/label/div/div[1]/div[2]/div[1]/input")))
-                continue
+                    driver.find_element(By.XPATH, "/html/body/div[2]/div/div/form/div[3]/div[4]/button").click()
+                    driver.switch_to.default_content()
+                    wait = WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div/form/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div/div/div[1]/label/div/div[1]/div[2]/div[1]/input")))
+                    continue
+                
+                except:
+                    
+                    #wait = WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[2]/iframe")))
+                    try:
+                        iframe = driver.find_element("xpath", "/html/body/div[2]/div/div[2]/iframe")
+                    except:
+                        wait = WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[2]/iframe")))
+                        iframe = driver.find_element("xpath", "/html/body/div[2]/div/div[2]/iframe")
+
+
+                    driver.switch_to.frame(iframe)
+                    wait = WebDriverWait(driver, 8).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div")))
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[1]/input').click()
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[1]/input').click()
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[1]/input').send_keys("timescantaufeek")
+
+                    #/html/body/div[2]/div/div/form/div[3]/div[2]/input
+
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[2]/input').click()
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[2]/input').click()
+                    driver.find_element("xpath", '/html/body/div[2]/div/div/form/div[3]/div[2]/input').send_keys("TIMESCAN")
+
+                    driver.find_element(By.XPATH, "/html/body/div[2]/div/div/form/div[3]/div[4]/button").click()
+                    driver.switch_to.default_content()
+                    wait = WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div/form/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div/div/div[1]/label/div/div[1]/div[2]/div[1]/input")))
+                    continue
+
             except:
-                print("solve captcha")
-                driver.get_in("https://solutions.hapag-lloyd.com/quick-quotes/#/")
-                wait = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div/form/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div/div/div[1]/label/div/div[1]/div[2]/div[1]/input")))
-                sleep(4)
-                continue
+                print("solve captcha and rerun")
+                quit()
 
 todays_date = date.today()
 two_weeks = todays_date + timedelta(weeks = 2)  
@@ -110,4 +139,4 @@ date_input = two_weeks_list[2] + "." + two_weeks_list[1] + "." + two_weeks_list[
 
 
 happag(sys.argv[1], sys.argv[2], date_input)
-#happag("Chennai", "hamburg", date_input)
+#happag("chennai", "norfolk", date_input)
